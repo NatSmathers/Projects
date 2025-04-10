ba5afcb2d88389bf857a8a9e821ade3614af1336
NatSmathers <nrsmathers@gmail.com>
2025-04-10 12:14:02 -0400

    Update CarFinder.py
    Project 0-4

--- CarFinder.py
+++ CarFinder.py
@@ -6,7 +6,7 @@ AllowedVehiclesList = [ 'Ford F-150',
 
 while True:
     print("""********************************
-AutoCountry Vehicle Finder v0.2
+AutoCountry Vehicle Finder v0.4
 ********************************""")
     
     menu = print("""Please Enter the following number below from the following menu: 
@@ -14,15 +14,16 @@ AutoCountry Vehicle Finder v0.2
 1. PRINT all Authorized Vehicles
 2. SEARCH for Authorized Vehicle
 3. ADD Authorized Vehicle
-4. Exit
+4. DELETE Authorized Vehicle
+5. Exit
 ********************************""")
 
     answer = input("Enter Number: ")
 
     if answer == "1":
         print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles: ")
-        for x in AllowedVehiclesList:
-            print(x)
+        for vehicle in AllowedVehiclesList:
+            print(vehicle)
     elif answer == "2":
         answer2 = input("Please Enter the full vehicle name: ")
         if answer2 in AllowedVehiclesList:
@@ -31,7 +32,13 @@ AutoCountry Vehicle Finder v0.2
             print(f'{answer2} is not an authorized vehicle, if you received this in error please check the spelling and try again')
     elif answer == "3":
         add = input("Please Enter the full Vehicle name you would like to add: ")
-        list = AllowedVehiclesList.append(add)
+        list1 = AllowedVehiclesList.append(add)
         print(f'You have added {add} as an authorized vehicle')
     elif answer == "4":
+        remove = input("Please Enter the full Vehicle name you would like to REMOVE: ")
+        sure = input(f'Are you sure you want to remove {remove} from the Authorized Vehicles List? ')
+        if sure == "yes":
+            list2 = AllowedVehiclesList.remove(remove)
+            print(f'You have REMOVED {remove} as an authorized vehicle')
+    elif answer == "5":
         exit("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
